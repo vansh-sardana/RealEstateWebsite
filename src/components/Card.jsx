25d570fc8969d9b1cab62dab8ee4d22483bdde5c
Vansh Sardana <107904311+vansh-sardana@users.noreply.github.com>
2024-09-14 16:37:49 +0530

    added more details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,11 +9,11 @@ const Card = ({post}) => {
     
     function addHandler(){
         dispatch(add(post));
-        toast.success("Item added to cart!");
+        // toast.success("Item added to cart!");
     }
     function removeHandler(){
         dispatch(remove(post.id));
-        toast.error("Item removed from cart!");
+        // toast.error("Item removed from cart!");
     }
 
     const [selected,setSelected]= useState(false);
@@ -22,11 +22,16 @@ const Card = ({post}) => {
             <h3 className='text-gray-700 font-semibold text-lg w-40 truncate mt-1'>{post?.agency?.name}</h3>
             <p className='w-40 text-normal text-gray-400 text-[10px]'>{post?._highlightResult?.title?.value?.split(" ").slice(0,10).join(" ")+" ..."}</p>
             <img src={post?.coverPhoto?.url} className='s:h-[200px] s:w-[200px] md:h-[300px] md:w-[300px]'/>
+            <div className='flex w-full justify-around'>
+                <p>Baths: {post?.baths}</p>
+                <p>Area: {Math.trunc(post?.area)} sq. m</p>
+                <p>{post?.location[0]?.name}</p>
+            </div>
             <div className='flex flex-row justify-between gap-11 mt-5 w-full items-center'>
                 <p className='text-green-600 font-semibold'>${post?.price}</p>
                 {
                 cart.some((p)=> p.id===post.id )? 
-                <button onClick={removeHandler} className='text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] uppercase py-1 px-3 group-hover:bg-gray-700 group-hover:text-white transition-all duration-300 ease-in-out'>Remove Item</button>
+                <button onClick={removeHandler} className='text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] uppercase py-1 px-3 group-hover:bg-gray-700 group-hover:text-white transition-all duration-300 ease-in-out'>{post?.product}</button>
                 : 
                 <button onClick={addHandler} className='text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] uppercase py-1 px-3 group-hover:bg-gray-700 group-hover:text-white transition-all duration-300 ease-in-out'>{(post?.availabilityStatus==="available")? "Available": "Not Available"}</button>}
                 
